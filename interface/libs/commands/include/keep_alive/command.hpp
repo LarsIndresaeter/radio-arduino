51ab299de4770e1d02c2d27d6f55b70644600d74
Lars Indresaeter <lars@lindresaeter.no>
2025-03-18 21:52:50 +0100

    interface: add keep alive command

---
+++ interface/libs/commands/include/keep_alive/command.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <common/uartCommandBase.hpp>
+
+class UartCommandKeepAlive : public UartCommandBase {
+public:
+    UartCommandKeepAlive(uint8_t time)
+        : UartCommandBase(
+            static_cast<uint8_t>(COMMANDS::OI::KEEP_ALIVE),
+            COMMANDS::KEEP_ALIVE::COMMAND_LENGTH)
+    {
+        m_payload.at(offsetof(COMMANDS::KEEP_ALIVE::command_t, time)) = time;
+    };
+
+    void print(std::ostream& out) const override
+    {
+        out << "KEEP_ALIVE           : ";
+        UartCommandBase::print(out);
+    };
+
+    COMMANDS::KEEP_ALIVE::response_t responseStruct()
+    {
+        return {(uint8_t*)&m_response.data()[PROTOCOL::HEADER::LENGTH]};
+    };
+};
+
