2bf86ad6e9d7df23797def192398a69adc7e1a2d
Lars Indresaeter <lars@lindresaeter.no>
2025-04-21 21:08:03 +0200

    interface: method for read and write to buffer

--- interface/libs/protocol/include/protocolBase.hpp
+++ interface/libs/protocol/include/protocolBase.hpp
@@ -79,6 +79,26 @@ public:
         *pCrc = tmp;
     }
 
+    uint32_t readUint32FromBuffer(uint8_t* buffer)
+    {
+        uint32_t retval = 0;
+
+        retval = (uint32_t) buffer[0] << 24;
+        retval |= (uint32_t) buffer[1] << 16;
+        retval |= (uint32_t) buffer[2] << 8;
+        retval |= (uint32_t) buffer[3];
+
+        return(retval);
+    }
+
+    void writeUint32ToBuffer(uint8_t* buffer, uint32_t value)
+    {
+        buffer[0] = (uint8_t)(value >> 24);
+        buffer[1] = (uint8_t)(value >> 16);
+        buffer[2] = (uint8_t)(value >> 8);
+        buffer[3] = (uint8_t)(value);
+    }
+
     void createPacket(uint8_t length, uint8_t* payload, uint8_t* packet, uint8_t protocol_version) // add isRadioFlag
     {
         uint32_t crc = 0;
@@ -95,10 +115,7 @@ public:
         }
         CRC32_calculate(
             (uint8_t*)&packet[PROTOCOL::PAYLOAD::PAYLOAD_OFFSET], length, &crc);
-        packet[PROTOCOL::HEADER::LENGTH + length] = crc >> 24;
-        packet[PROTOCOL::HEADER::LENGTH + length + 1] = crc >> 16;
-        packet[PROTOCOL::HEADER::LENGTH + length + 2] = crc >> 8;
-        packet[PROTOCOL::HEADER::LENGTH + length + 3] = crc;
+        writeUint32ToBuffer(&packet[PROTOCOL::HEADER::LENGTH + length], crc);
     }
 
     void createEncryptedPacket(
@@ -115,22 +132,9 @@ public:
 
         // create header which act as initialization vector
         uint32_t nonce = m_cryptoHandler->nonce();
-        packet[PROTOCOL::ENCRYPTED::NONCE_OFFSET] = nonce >> 24;
-        packet[PROTOCOL::ENCRYPTED::NONCE_OFFSET + 1] = nonce >> 16;
-        packet[PROTOCOL::ENCRYPTED::NONCE_OFFSET + 2] = nonce >> 8;
-        packet[PROTOCOL::ENCRYPTED::NONCE_OFFSET + 3] = nonce;
-
-        packet[PROTOCOL::ENCRYPTED::RESERVED_OFFSET] = 0;
-        packet[PROTOCOL::ENCRYPTED::RESERVED_OFFSET + 1] = 0;
-        packet[PROTOCOL::ENCRYPTED::RESERVED_OFFSET + 2] = 0;
-        packet[PROTOCOL::ENCRYPTED::RESERVED_OFFSET + 3] = 0;
-
-        packet[PROTOCOL::ENCRYPTED::MESSAGE_ID_OFFSET] = m_lastMessageId >> 24;
-        packet[PROTOCOL::ENCRYPTED::MESSAGE_ID_OFFSET + 1]
-            = m_lastMessageId >> 16;
-        packet[PROTOCOL::ENCRYPTED::MESSAGE_ID_OFFSET + 2]
-            = m_lastMessageId >> 8;
-        packet[PROTOCOL::ENCRYPTED::MESSAGE_ID_OFFSET + 3] = m_lastMessageId;
+        writeUint32ToBuffer(&packet[PROTOCOL::ENCRYPTED::NONCE_OFFSET], nonce);
+        writeUint32ToBuffer(&packet[PROTOCOL::ENCRYPTED::RESERVED_OFFSET], 0);
+        writeUint32ToBuffer(&packet[PROTOCOL::ENCRYPTED::MESSAGE_ID_OFFSET], m_lastMessageId);
         m_lastMessageId++;
 
         // copy payload
@@ -140,10 +144,7 @@ public:
 
         // inner checksum of nonce, message_id and message
         uint32_t checksum = m_cryptoHandler->checksum(length + 12, &packet[8]);
-        packet[PROTOCOL::ENCRYPTED::CHECKSUM_OFFSET] = checksum >> 24;
-        packet[PROTOCOL::ENCRYPTED::CHECKSUM_OFFSET + 1] = checksum >> 16;
-        packet[PROTOCOL::ENCRYPTED::CHECKSUM_OFFSET + 2] = checksum >> 8;
-        packet[PROTOCOL::ENCRYPTED::CHECKSUM_OFFSET + 3] = checksum;
+        writeUint32ToBuffer(&packet[PROTOCOL::ENCRYPTED::CHECKSUM_OFFSET], checksum);
 
         // encrypt inner checksum, nonce, message_id and message
         m_cryptoHandler->encrypt(
@@ -151,30 +152,20 @@ public:
 
         // add message authentification code
         uint32_t mac = m_cryptoHandler->mac(length + 16, &packet[4]);
-        packet[PROTOCOL::ENCRYPTED::CRYPTO_OVERHEAD + length] = mac >> 24;
-        packet[PROTOCOL::ENCRYPTED::CRYPTO_OVERHEAD + length + 1] = mac >> 16;
-        packet[PROTOCOL::ENCRYPTED::CRYPTO_OVERHEAD + length + 2] = mac >> 8;
-        packet[PROTOCOL::ENCRYPTED::CRYPTO_OVERHEAD + length + 3] = mac;
+        writeUint32ToBuffer(&packet[PROTOCOL::ENCRYPTED::CRYPTO_OVERHEAD + length], mac);
 
         CRC32_calculate(
             (uint8_t*)&packet[PROTOCOL::PAYLOAD::PAYLOAD_OFFSET],
             length + PROTOCOL::ENCRYPTED::CRYPTO_OVERHEAD,
             &crc);
-        packet[PROTOCOL::HEADER::LENGTH + PROTOCOL::ENCRYPTED::CRYPTO_OVERHEAD + length] = crc >> 24;
-        packet[PROTOCOL::HEADER::LENGTH + PROTOCOL::ENCRYPTED::CRYPTO_OVERHEAD + length + 1] = crc >> 16;
-        packet[PROTOCOL::HEADER::LENGTH + PROTOCOL::ENCRYPTED::CRYPTO_OVERHEAD + length + 2] = crc >> 8;
-        packet[PROTOCOL::HEADER::LENGTH + PROTOCOL::ENCRYPTED::CRYPTO_OVERHEAD + length + 3] = crc;
+        writeUint32ToBuffer(&packet[PROTOCOL::HEADER::LENGTH + PROTOCOL::ENCRYPTED::CRYPTO_OVERHEAD + length], crc);
     }
 
     uint8_t decryptPayload(uint8_t length, uint8_t* payload)
     {
         // validate mac
         uint32_t mac_calculated = m_cryptoHandler->mac(length - 4, payload);
-        uint32_t mac_package;
-        mac_package = ((uint32_t)payload[length - 4]) << 24;
-        mac_package |= ((uint32_t)payload[length - 3]) << 16;
-        mac_package |= ((uint32_t)payload[length - 2]) << 8;
-        mac_package |= ((uint32_t)payload[length - 1]);
+        uint32_t mac_package = readUint32FromBuffer(&payload[length - 4]);
 
         if (mac_calculated != mac_package) {
             return 0;
@@ -186,17 +177,8 @@ public:
         // validate checksum
         uint32_t checksum_calculated
             = m_cryptoHandler->checksum(length - 8, &payload[4]);
-        uint32_t checksum_package;
-        checksum_package = ((uint32_t)payload[0]) << 24;
-        checksum_package |= ((uint32_t)payload[1]) << 16;
-        checksum_package |= ((uint32_t)payload[2]) << 8;
-        checksum_package |= ((uint32_t)payload[3]);
-
-        uint32_t message_id;
-        message_id = ((uint32_t)payload[4]) << 24;
-        message_id |= ((uint32_t)payload[5]) << 16;
-        message_id |= ((uint32_t)payload[6]) << 8;
-        message_id |= ((uint32_t)payload[7]);
+        uint32_t checksum_package = readUint32FromBuffer(&payload[0]);
+        uint32_t message_id = readUint32FromBuffer(&payload[4]);;
 
         if (message_id > m_lastMessageId) {
             m_lastMessageId = message_id;
