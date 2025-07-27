4e647dc2e9eac2e80518e92f2f7ab1b3f59ec131
Lars Indresaeter <lars@lindresaeter.no>
2025-07-27 18:51:49 +0200

    interface: rename conan packages

--- interface/test_package/conanfile.py
+++ interface/test_package/conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake, tools
 from conans.tools import os_info, SystemPackageTool
 
 class AvrUartConan(ConanFile):
-    name = "uart-api-test"
+    name = "raduino-api-test"
     version = "0.0.1"
     license = "gpl"
     author = "Lars Indresaeter"
@@ -15,17 +15,17 @@ class AvrUartConan(ConanFile):
             "CMakeLists.txt",
             "*.cpp",
             ]
-    test_type = "uart-api/0.0.1@lars/test"
+    test_type = "raduino-api/0.0.1@lars/test"
 
     def requirements(self):
         self.requires("gtest/1.8.1")
-        self.requires("uart-api/0.0.1@lars/test",private=True)
+        self.requires("raduino-api/0.0.1@lars/test",private=True)
 
     def build_requirements(self):
         self.build_requires("gtest/1.8.1", force_host_context=True)
 
     def imports(self):
-        self.copy("*.hpp", dst="libs", src="libs", root_package="uart-api")
+        self.copy("*.hpp", dst="libs", src="libs", root_package="raduino-api")
 
     def build(self):
         cmake = CMake(self)
