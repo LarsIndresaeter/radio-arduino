15d49db7f97949036a33c8c25a04e5ea862be3b5
Lars Indresaeter <lars@lindresaeter.no>
2025-10-12 09:58:09 +0200

    update test script

--- tools/helper_scripts/test.sh
+++ tools/helper_scripts/test.sh
@@ -6,13 +6,27 @@ echo "gateway on ttyUSB0 ========================"
 
 echo "switch roles =============================="
 ./bin/raduino-node --device /dev/ttyUSB0 -n 0 -g
-./bin/raduino-gateway --device /dev/ttyUSB0 -n
+if [ "$(./bin/raduino-node --device /dev/ttyUSB1 -n 0 -p | grep PING)" == "complete : PING" ]
+then
+    exit "failed to switch role of radio node"
+    exit 1
+else
+    #echo "node successfully switched mode to gateway. Switch other gateway to node"
+    ./bin/raduino-gateway --device /dev/ttyUSB0 -n
+fi
 
-echo "gateway on ttyUSB1 ========================"
-./bin/raduino-gateway --device /dev/ttyUSB1 -xz
-./bin/raduino-node --device /dev/ttyUSB1 -n 0 -Nz
+echo "gateway on ttyUSB1 ========================" ./bin/raduino-gateway --device /dev/ttyUSB1 -xz
+./bin/raduino-gateway --device /dev/ttyUSB0 -n 0 -xz
+./bin/raduino-node --device /dev/ttyUSB0 -n 0 -Nz
 
 echo "switch roles =============================="
 ./bin/raduino-node --device /dev/ttyUSB1 -n 0 -g
-./bin/raduino-gateway --device /dev/ttyUSB1 -n
+if [ "$(./bin/raduino-node --device /dev/ttyUSB0 -n 0 -p | grep PING)" == "complete : PING" ]
+then
+    exit "failed to switch role of radio node"
+    exit 1
+else
+    #echo "node successfully switched mode to gateway. Switch other gateway to node"
+    ./bin/raduino-gateway --device /dev/ttyUSB1 -n
+fi
 
