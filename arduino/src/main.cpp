8c5db1054e72fe8e0f331f53313cbb249db11ae9
Lars Indresaeter <lars@lindresaeter.no>
2025-10-12 22:06:42 +0200

    arduino: implement soft reset command

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -606,6 +606,15 @@ void commandSetRadioRole(uint8_t* commandPayload, uint8_t* responsePayload)
     response.serialize(responsePayload);
 }
 
+void commandSoftReset(uint8_t* commandPayload, uint8_t* responsePayload)
+{
+    COMMANDS::SOFT_RESET::command_t command(commandPayload);
+    COMMANDS::SOFT_RESET::response_t response;
+
+    WATCHDOG::softReset();
+    response.serialize(responsePayload);
+}
+
 void commandSwitch(uint8_t* commandPayload, uint8_t* responsePayload, ComBusInterface* comBus)
 {
     uint8_t cmd_id = commandPayload[0];
@@ -721,6 +730,9 @@ void commandSwitch(uint8_t* commandPayload, uint8_t* responsePayload, ComBusInte
         break;
     case COMMANDS::OI::SET_RADIO_ROLE:
         commandSetRadioRole(commandPayload, responsePayload);
+        break;
+    case COMMANDS::OI::SOFT_RESET:
+        commandSoftReset(commandPayload, responsePayload);
         break;
     default:
         break;
