7ab240f839eba1b7b0c914315e8303cb03a06207
Lars Indresaeter <lars@lindresaeter.no>
2025-11-16 19:24:31 +0100

    feat(docker): add .bashrc for docker container

--- tools/scripts/devbox.sh
+++ tools/scripts/devbox.sh
@@ -21,7 +21,7 @@ then
     docker rmi -f radio-arduino-build
 elif [ "${PARAM}" == "run" ]
 then
-    docker run ${USER_STRING} -v ${REPO_BASE_DIR}/:/home/raduino/ -it $CONTAINER_NAME /usr/bin/bash -c 'source tools/setup.sh local'
+    docker run ${USER_STRING} -v ${REPO_BASE_DIR}/:/home/raduino/ -it $CONTAINER_NAME /usr/bin/bash --rcfile tools/docker/.bashrc
 else
     echo "valid parameters: clean,build,run"
 fi
