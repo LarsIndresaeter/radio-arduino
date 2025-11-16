ebfa31f5e3d21fcf23e118be0209df733b6a908e
Lars Indresaeter <lars@lindresaeter.no>
2025-11-16 20:46:12 +0100

    feat: fix paho-mqtt-cpp package reference

--- linux/conanfile.py
+++ linux/conanfile.py
@@ -46,7 +46,7 @@ class AvrUartConan(ConanFile):
 
     def requirements(self):
         if self.options.mqtt == True:
-            self.requires("paho-mqtt-cpp/1.2.0@", private=True)
+            self.requires("paho-mqtt-cpp/1.4.0", private=True)
             self.requires("nlohmann_json/3.11.3", private=True)
         else:
             self.requires("openssl/3.1.1",private=True)
