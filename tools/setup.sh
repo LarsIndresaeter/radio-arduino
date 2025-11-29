2eb0076d49790b75ff8547fd2cc650ace56115a4
Lars Indresaeter <lars.indresaeter@gmail.com>
2025-11-29 12:57:23 +0100

    feat: alert if conan is not installed

--- tools/setup.sh
+++ tools/setup.sh
@@ -4,29 +4,32 @@ SCRIPT_PATH="${PWD}/tools/setup.sh"
 
 SOURCE_COMMAND=0
 
-# source command on linux
-if [ "$0" == "-bash" ]
-then
-    SOURCE_COMMAND=1
-fi
+if command -v conan &> /dev/null; then
+    # source command on linux
+    if [ "$0" == "-bash" ]
+    then
+        SOURCE_COMMAND=1
+    fi
 
-# source command in docker
-if [ "$0" == "bash" ]
-then
-    SOURCE_COMMAND=1
-fi
+    # source command in docker
+    if [ "$0" == "bash" ]
+    then
+        SOURCE_COMMAND=1
+    fi
 
-if [ "$0" == "/usr/bin/bash" ]
-then
-    SOURCE_COMMAND=1
-fi
+    if [ "$0" == "/usr/bin/bash" ]
+    then
+        SOURCE_COMMAND=1
+    fi
 
-if [ "$SOURCE_COMMAND" != 1 ]
-then
-    echo "usage: source ${SCRIPT_PATH}"
+    if [ "$SOURCE_COMMAND" != 1 ]
+    then
+        echo "usage: source ${SCRIPT_PATH}"
+    else
+        . tools/scripts/export_environment.sh
+        alias "${RADINO_CUSTOM_COMMAND}"="${RADINO_TOOLS_DIR}/toolkit.sh"
+        echo "${RADINO_CUSTOM_COMMAND} command ready"
+    fi
 else
-    . tools/scripts/export_environment.sh
-    alias "${RADINO_CUSTOM_COMMAND}"="${RADINO_TOOLS_DIR}/toolkit.sh"
-    echo "${RADINO_CUSTOM_COMMAND} command ready"
+    echo "conan is not installed"
 fi
-
