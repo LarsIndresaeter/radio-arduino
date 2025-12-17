2734a0007f4d58cccc9315a2bc60f4f5ffec0b18
Lars Indresaeter <lars.indresaeter@gmail.com>
2025-12-17 22:07:52 +0100

    refactor: merge flash.sh with rf-nano_flash.sh

--- tools/scripts/flash.sh
+++ tools/scripts/flash.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 
+_SERIAL_DEVICE_GATEWAY=/dev/ttyUSB0
+_SERIAL_DEVICE_NODE=/dev/ttyUSB1
+
 PARAM=$1
+
+flash_gateway_nano_pro()
+{
+    avrdude -c arduino -b 115200 -P ${_SERIAL_DEVICE_GATEWAY} -p atmega328p -vv -U flash:w:bin/arduino.hex
+}
+
+flash_node_nano_pro()
+{
+    avrdude -c arduino -b 115200 -P ${_SERIAL_DEVICE_NODE} -p atmega328p -vv -U flash:w:bin/arduino.hex
+}
+
+nano-pro_flash()
+{
+    local _DEVICE=$1
+
+    if [ "${PARAM}" == "node" ] 
+    then
+        flash_node_nano_pro
+    elif [ "${PARAM}" == "gateway" ] 
+    then
+        flash_gateway_nano_pro
+    elif [ "${PARAM}" == "both" ] 
+    then
+        flash_node_nano_pro
+        flash_gateway_nano_pro
+    else 
+        echo "missing parameter: node, gateway or both"
+    fi
+}
 
 if [ "${PARAM}" == "nano-pro" ]
 then
