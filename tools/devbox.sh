6baef69c042309f813c50abbcbc6dfb02ce98632
Lars Indresaeter <lars.indresaeter@gmail.com>
2025-12-22 23:55:24 +0100

    fix: use radio-arduino-devbox from ghcr.io to build

--- tools/devbox.sh
+++ tools/devbox.sh
@@ -4,7 +4,7 @@ MY_USER_ID=$(id -u)
 MY_GROUP_ID=$(id -g)
 USER_STRING="--user ${MY_USER_ID}:${MY_GROUP_ID}"
 REPO_BASE_DIR=$(git rev-parse --show-toplevel)
-CONTAINER_NAME=radio-arduino-build
+CONTAINER_NAME=ghcr.io/larsindresaeter/radio-arduino-devbox:1.0.0
 
 PARAM=$1
 if [ "${PARAM}" == "build" ]
@@ -18,7 +18,7 @@ then
     rm -fr .cache/
     rm -f .bash_history
     echo "remove old image"
-    docker rmi -f radio-arduino-build
+    docker rmi -f radio-arduino-devbox
 elif [ "${PARAM}" == "run" ]
 then
     docker run ${USER_STRING} -v ${REPO_BASE_DIR}/:/home/raduino/ -it $CONTAINER_NAME /usr/bin/bash --rcfile tools/docker/.bashrc
