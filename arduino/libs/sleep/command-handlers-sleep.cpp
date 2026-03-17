e2fa81a8b596e586d5819f95c95083f5cbdc43e5
Lars Indresaeter <lars@lindresaeter.no>
2026-03-17 18:34:22 +0100

    refactor: arduino

--- arduino/libs/sleep/command-handlers-sleep.cpp
+++ arduino/libs/sleep/command-handlers-sleep.cpp
@@ -22,41 +22,18 @@ void commandGetActiveTimeCounter(uint8_t* commandPayload, uint8_t* responsePaylo
     COMMANDS::GET_ACTIVE_TIME_COUNTER::command_t command(commandPayload);
     COMMANDS::GET_ACTIVE_TIME_COUNTER::response_t response;
 
-    uint64_t timestamp = (uint64_t)(command.timestamp[0]);
-    timestamp += (uint64_t)(command.timestamp[1]) << 8;
-    timestamp += (uint64_t)(command.timestamp[2]) << 16;
-    timestamp += (uint64_t)(command.timestamp[3]) << 24;
-    timestamp += (uint64_t)(command.timestamp[4]) << 32;
-    timestamp += (uint64_t)(command.timestamp[5]) << 40;
-    timestamp += (uint64_t)(command.timestamp[6]) << 48;
-    timestamp += (uint64_t)(command.timestamp[7]) << 56;
+    uint64_t timestamp = command.getTimestamp();
 
     if(sleep_time_start_measurement == 0)
     {
-        // reset sleep_time measurement and record the time
         sleep_time_start_measurement = timestamp;
         sleep_time = 0;
     }
 
     uint64_t active_time = timestamp - sleep_time_start_measurement - sleep_time;
 
-    response.active_time[7] = active_time;
-    response.active_time[6] = active_time >> 8;
-    response.active_time[5] = active_time >> 16;
-    response.active_time[4] = active_time >> 24;
-    response.active_time[3] = active_time >> 32;
-    response.active_time[2] = active_time >> 40;
-    response.active_time[1] = active_time >> 48;
-    response.active_time[0] = active_time >> 56;
-
-    response.sleep_time[7] = sleep_time;
-    response.sleep_time[6] = sleep_time >> 8;
-    response.sleep_time[5] = sleep_time >> 16;
-    response.sleep_time[4] = sleep_time >> 24;
-    response.sleep_time[3] = sleep_time >> 32;
-    response.sleep_time[2] = sleep_time >> 40;
-    response.sleep_time[1] = sleep_time >> 48;
-    response.sleep_time[0] = sleep_time >> 56;
+    response.setActive_time(active_time);
+    response.setSleep_time(sleep_time);
 
     response.serialize(responsePayload);
 }
